6360427058ecb5d56d0820ca7f37bd011c191a71
hild365 <144734534+hild365@users.noreply.github.com>
2024-05-16 08:36:26 +0200

    Update StrategieJeuDeNimGagnante.java

--- src/src/modele/jeuNim/StrategieJeuDeNimGagnante.java
+++ src/src/modele/jeuNim/StrategieJeuDeNimGagnante.java
@@ -19,7 +19,7 @@ public class StrategieJeuDeNimGagnante implements Strategie {
             for (int i = 1; i <= etatPartieJN.getNbTas(); i++) {
                 if (etatPartieJN.getNbAllumettes(i) > 0) {
                     CoupJeuDeNim coup = new CoupJeuDeNim();
-                    coup.setNumJoueur(2); // En supposant que l'IA est le joueur 2
+                    coup.setNumJoueur(2); // l'IA est le joueur 2
                     coup.setNumTas(i);
                     coup.setNbAllu(1);
                     return coup;
@@ -30,7 +30,7 @@ public class StrategieJeuDeNimGagnante implements Strategie {
                 int nb = etatPartieJN.getNbAllumettes(i) ^ resultatXor;
                 if (nb < etatPartieJN.getNbAllumettes(i)) {
                     CoupJeuDeNim coup = new CoupJeuDeNim();
-                    coup.setNumJoueur(2); // En supposant que l'IA est le joueur 2
+                    coup.setNumJoueur(2); // l'IA est le joueur 2
                     coup.setNumTas(i);
                     coup.setNbAllu(etatPartieJN.getNbAllumettes(i) - nb);
                     return coup;
